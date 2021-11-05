60c57e93b618680f7542e83eaee060d2c7d5b6b1
kamranuz <78980405+kamranuz@users.noreply.github.com>
2021-11-05 23:38:16 +0300

    Added new functions in Grapher class
    Added realization for function scatter3d() 
    Added placeholder for function show_separate()

--- classes/graphics.py
+++ classes/graphics.py
@@ -1,12 +1,13 @@
 class Grapher:
     def __init__(self, 
                  train, test=[], target_col = 'default_12m',
-                 figsize = (8,4), target_colors = {0:'gray',1:'red'}
+                 figsize = (10,5), target_colors = {0:'gray',1:'red'}
                  ):         
 
         self.df_original = (train,test)
         self.target = train[target_col]
         self.df = train.drop(columns=[target_col])
+        self.dfs_by_target = [train[train[target_col]==target_value] for target_value in train[target_col].unique()]
 
         self.test = test
 
@@ -16,28 +17,48 @@ class Grapher:
                           self.scatter3d:3,
                           self.hist:1}
     
-    def show_all(self,f_plt):
+    def show_all(self,fplt):
         '''
-        shows all possible f_plt plots of df
+        shows all possible fplt plots of df
         Example:
             g = Grapher( pd.DataFrame({a:[],b:[],c:[]}) )
             g.show_all(g.scatter) 
             -> 
             prints all possible scatter plots:
             g.scatter(a,b);g.scatter(a,c);g.scatter(b,c)
-
         !: 
             if many df columns then there are too many graphs to show
         '''
 
         from itertools import combinations as combi
-        n=self.fplt_dims[f_plt]
+        n=self.fplt_dims[fplt]
 
         for dims in combi(self.df.columns,n):
-            f_plt(*dims)
+            fplt(*dims)
 
+    def show_separate(self, fplt):
+        '''
+        should show 3 fplt plots side by side: 
+        {{default+alive,default,alive}}
+
+        In general should show n+1 fplt plots side by side:
+        {{everething, target_group_1, target_group_2, ... , target_group_n}}
+        '''
+        # all = self.df
+        # dead = all[self.target == 1]
+        # alive = all[all[self.target != 1]
+
+        # all = Grapher(train=all)
+        # dead = Grapher(train=dead)
+        # alive = Grapher(train=alive)
+
+        # all.scatter3d()
+        # dead.scatter3d()
+        # alive.scatter3d()
+        pass
+    
     def scatter(self,
-                x_name = 'ar_management_expenses', y_name = 'ar_net_profit',
+                x_name = 'bus_age', y_name = 'ul_capital_sum',
                 alpha = 0.3,
                 xscale = 'linear',
                 yscale = 'linear'
@@ -58,8 +79,32 @@ class Grapher:
 
         plt.show()
 
-    def scatter3d(self):
-        pass
+    def scatter3d(self,
+                  x_name='ul_capital_sum',
+                  y_name='ul_founders_cnt',
+                  z_name='bus_age',
+                  alpha = 0.3,
+                  xscale = 'linear',
+                  yscale = 'linear',
+                  zscale = 'linear'):
+        
+        fig = plt.figure(figsize=self.plt_figsize)
+        ax = fig.add_subplot(projection='3d')
+
+        ax.scatter(self.df[x_name], 
+                   self.df[y_name], 
+                   self.df[z_name], 
+                   c = self.plt_colored_target,
+                   alpha = alpha)
+        
+        ax.set_xscale(xscale)
+        ax.set_yscale(yscale)
+        ax.set_zscale(zscale)
+        ax.set_xlabel(x_name)
+        ax.set_ylabel(y_name)
+        ax.set_zlabel(z_name)
+
+        plt.show()
 
     def hist(self, n_bins = 10):
         pass
